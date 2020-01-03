5862793eb7146dd4ef3d7efb45c34b9189f1711b
haruosuz <haruo@g-language.org>
2020-01-03 16:55:29 +0900

    update r4ds/scripts/

---
+++ r4ds/scripts/my_r4ds_1_Introduction.R
@@ -0,0 +1,15 @@
+# http://r4ds.had.co.nz/introduction.html
+# 1 Introduction
+
+# 1.4.3 The tidyverse
+#install.packages("tidyverse")
+library(tidyverse)
+
+# 1.4.4 Other packages
+#install.packages(c("nycflights13", "gapminder", "Lahman"))
+
+# 1.5 Running R code
+1 + 2
+
+# 1.8 Colophon
+devtools::session_info(c("tidyverse"))
---
+++ r4ds/scripts/my_r4ds_5_Data_transformation.R
@@ -0,0 +1,28 @@
+# https://r4ds.had.co.nz/transform.html
+# 5 Data transformation
+## 5.1 Introduction
+### 5.1.1 Prerequisites
+library(nycflights13)
+library(tidyverse)
+### 5.1.2 nycflights13
+flights
+View(flights)
+
+## 5.2 Filter rows with filter()
+## 3.2　filter()で行にフィルタにかける
+
+### 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
